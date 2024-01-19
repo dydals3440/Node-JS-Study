a0c0e77e75976da016bd003cc51dacaee39528a2
김용민 <dydals3440@gmail.com>
2024-01-19 20:49:43 +0900

    refactor: file storage code refactor

--- MVC Pattern/models/product.js
+++ MVC Pattern/models/product.js
@@ -2,6 +2,24 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  // 비동기식 코드.
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      // return [];
+      return cb([]);
+    }
+    // return JSON.parse(fileContent);
+    cb(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
@@ -9,16 +27,7 @@ module.exports = class Product {
 
   save() {
     // products.push(this);
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -29,19 +38,6 @@ module.exports = class Product {
   static fetchAll(cb) {
     // 에러 발생, 그냥 products를 반환해야하는데 클래스의 로컬 속성이 아니라  1번줄의 배열을 반환하고 있어서 그렇다.
     // return this.products;
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    // 비동기식 코드.
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        // return [];
-        cb([]);
-      }
-      // return JSON.parse(fileContent);
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
