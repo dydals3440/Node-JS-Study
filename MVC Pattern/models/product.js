7d9ce626a9cef7fe564fea57adea36a8068584c5
김용민 <dydals3440@gmail.com>
2024-01-19 20:38:33 +0900

    fix: 모델을 통해 파일에서 데이터 가져오기

--- MVC Pattern/models/product.js
+++ MVC Pattern/models/product.js
@@ -1,4 +1,6 @@
-const products = [];
+// const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
   constructor(t) {
@@ -6,13 +8,40 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    // products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
+  static fetchAll(cb) {
     // 에러 발생, 그냥 products를 반환해야하는데 클래스의 로컬 속성이 아니라  1번줄의 배열을 반환하고 있어서 그렇다.
     // return this.products;
-
-    return products;
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    // 비동기식 코드.
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        // return [];
+        cb([]);
+      }
+      // return JSON.parse(fileContent);
+      cb(JSON.parse(fileContent));
+    });
   }
 };
