d15c19431b54374e88c36ed358258cac620a42e4
김용민 <dydals3440@gmail.com>
2024-01-21 18:38:49 +0900

    feat: Store Product Data

--- MVC Pattern/controllers/admin.js
+++ MVC Pattern/controllers/admin.js
@@ -11,7 +11,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title); // name title input
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(title, imageUrl, description, price); // name title input
   product.save();
   //   products.push({ title: req.body.title });
   res.redirect('/');
