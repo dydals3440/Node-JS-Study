b19c6319ee5810117fa196d60e765bfb6db9a2a4
김용민 <dydals3440@gmail.com>
2024-01-29 18:16:30 +0900

    feat: 제품 삭제 기능 추가

--- MVC Pattern/routes/admin.js
+++ MVC Pattern/routes/admin.js
@@ -19,4 +19,7 @@ router.get('/edit-product/:productId', adminController.getEditProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
 
+//  Post 요청이기에, URL 경로의 정보를 암호화하지 않는 대신, 요청 본문의 일부로 넣으면 된다.
+router.post('/delete-product', adminController.postDeleteProduct);
+
 module.exports = router;
